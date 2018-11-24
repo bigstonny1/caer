d34744621ef55188d13603bb141c9366e0377fff
bigstonny1 <43025240+bigstonny1@users.noreply.github.com>
2018-11-23 23:18:44 -0200

    Create footer.php

---
+++ view/footer.php
@@ -0,0 +1,42 @@
+<!--IKLAN FOOTER PASTE DISINI-->
+<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				
+				<center>
+				    <div class="alert alert-info alert-dismissable">
+					<i class="fa fa-info-circle"></i> <strong>Aviso: </strong> <strong> Nós não estamos hospedando nenhum vídeo.</strong>
+				</center>
+				<div>
+<div class="row">
+			<center>
+				Copyright &copy; www.cinemaemcasa.online
+			</center>
+		</div>
+	</div>
+	</div>
+<script src="assets/js/jquery.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<div id='Back-to-top'>
+<img alt='Scroll to top' src='https://lh6.googleusercontent.com/-18ivhAksPaU/Uqfnr_rXbbI/AAAAAAAAIzw/klENnMeM290/s128/backtotop.png'/>
+</div>
+
+<script type='text/javascript'>
+$(function() { $(window).scroll(function() {
+    if($(this).scrollTop()>400) {
+        $('#Back-to-top').fadeIn();
+        }
+    else { $('#Back-to-top').fadeOut();}
+    });
+    $('#Back-to-top').click(function() {
+        $('body,html')
+        .animate({scrollTop:0},300)
+        .animate({scrollTop:40},200)
+        .animate({scrollTop:0},130)
+        .animate({scrollTop:15},100)
+        .animate({scrollTop:0},70);
+        });
+});
+</script>
+</body>
+</html>
